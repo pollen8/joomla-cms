b6e15f0a134581c9bbc5efc4967cebd733d85efa
Rob Clayburn <rob@pollen-8.co.uk>
2012-01-03 16:34:01 +0100

    moved cms form and formfield loads into application render() method

--- administrator/includes/application.php
+++ administrator/includes/application.php
@@ -173,6 +173,9 @@ class JAdministrator extends JApplication
 	 */
 	public function render()
 	{
+		JForm::addFieldPath(JPATH_PLATFORM . '/cms/form/field');
+		JForm::addRulePath(JPATH_PLATFORM . '/cms/form/rule');
+		
 		$component	= JRequest::getCmd('option', 'com_login');
 		$template	= $this->getTemplate(true);
 		$file		= JRequest::getCmd('tmpl', 'index');

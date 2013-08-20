767c61a9c01c0cef9b87c414abde188c4409d694
pollen8 <rob@pollen-8.co.uk>
2013-08-20 16:01:12 +0200

    clean up uneeded guff

--- libraries/joomla/form/fields/repeatable.php
+++ libraries/joomla/form/fields/repeatable.php
@@ -44,7 +44,6 @@ class JFormFieldRepeatable extends JFormField
 		// Initialize variables.
 		$app = JFactory::getApplication();
 		$document = JFactory::getDocument();
-		$options = array();
 		JHtml::stylesheet('administrator/components/com_fabrik/views/fabrikadmin.css');
 		$subForm = new JForm($this->name, array('control' => 'jform'));
 		$xml = $this->element->children()->asXML();
@@ -53,32 +52,16 @@ class JFormFieldRepeatable extends JFormField
 		// Needed for repeating modals in gmaps viz
 		$subForm->repeatCounter = (int) @$this->form->repeatCounter;
 		$input = $app->input;
-		$view = $input->get('view', 'item');
 		$children = $this->element->children();
 
-		// This fires a strict standard warning deep in JForm, suppress error for now
-		@$subForm->setFields($children);
+		$subForm->setFields($children);
 
 		$str = array();
 		$modalid = $this->id . '_modal';
 
-		// As JForm will render child fieldsets we have to hide it via CSS
-		if ($view != 'plugin')
-		{
-			$fieldSetId = str_replace('jform_params_', '', $modalid);
-		}
-		else
-		{
-			//Plugins fieldsets have 'options-' prefixed to them
-			$fieldSetId = str_replace('jform_params_', 'options-', $modalid);
-		}
-		$css = '#' . $fieldSetId . ' { display: none; }';
-		$document->addStyleDeclaration($css);
-
-		$path = 'templates/' . $app->getTemplate() . '/images/menu/';
 		$str[] = '<div id="' . $modalid . '" style="display:none">';
 		$str[] = '<table class="adminlist ' . $this->element['class'] . ' table table-striped">';
-		$str[] = '<thead><tr class="row0">';
+		$str[] = '<thead><tr>';
 		$names = array();
 		$attributes = $this->element->attributes();
 
@@ -90,7 +73,7 @@ class JFormFieldRepeatable extends JFormField
 			$str[] = '</th>';
 		}
 
-		$str[] = '<th><a href="#" class="add btn button btn-success"><i class="icon-plus"></i> </a></th>';
+		$str[] = '<th><a href="#" class="add btn button btn-success"><span class="icon-plus"></span> </a></th>';
 		$str[] = '</tr></thead>';
 		$str[] = '<tbody><tr>';
 
@@ -100,72 +83,26 @@ class JFormFieldRepeatable extends JFormField
 		}
 
 		$str[] = '<td>';
-		$str[] = '<div class="btn-group"><a class="add btn button btn-success"><i class="icon-plus"></i> </a>';
-		$str[] = '<a class="remove btn button btn-danger"><i class="icon-minus"></i> </a></div>';
+		$str[] = '<div class="btn-group"><a class="add btn button btn-success"><span class="icon-plus"></span> </a>';
+		$str[] = '<a class="remove btn button btn-danger"><span class="icon-minus"></span> </a></div>';
 		$str[] = '</td>';
 		$str[] = '</tr></tbody>';
 		$str[] = '</table>';
 		$str[] = '</div>';
-		$form = implode("\n", $str);
-		static $modalrepeat;
-		if (!isset($modalrepeat))
-		{
-			$modalrepeat = array();
-		}
-		if (!array_key_exists($modalid, $modalrepeat))
-		{
-			$modalrepeat[$modalid] = array();
-		}
-		if (!isset($this->form->repeatCounter))
-		{
-			$this->form->repeatCounter = 0;
-		}
-		if (!array_key_exists($this->form->repeatCounter, $modalrepeat[$modalid]))
-		{
-			// If loaded as js template then we don't want to repeat this again. (fabrik)
-			$names = json_encode($names);
-			$pane = str_replace('jform_params_', '', $modalid) . '-options';
 
-			$modalrepeat[$modalid][$this->form->repeatCounter] = true;
-			$script = str_replace('-', '', $modalid) . " = new FabrikModalRepeat('$modalid', $names, '$this->id');";
+		$names = json_encode($names);
 
-			$script = "(function ($){
-				$(document).ready(function (){
-					var repeatable = new $.JRepeatable('$modalid', $names, '$this->id');
-					console.log(repeatable);
-				});
-			})(jQuery);";
-			$option = $input->get('option');
+		JHTML::_('script', 'system/repeatable.js', true, true);
 
-			$context = strtoupper($option);
-			if ($context === 'COM_ADVANCEDMODULES')
-			{
-				$context = 'COM_MODULES';
-			}
-			$j3pane = $context . '_' . str_replace('jform_params_', '', $modalid) . '_FIELDSET_LABEL';
+		$script = "(function ($){
+			$(document).ready(function (){
+				var repeatable = new $.JRepeatable('$modalid', $names, '$this->id');
+			});
+		})(jQuery);";
 
-			/* $script .= "window.addEvent('domready', function() {
-				var a = jQuery(\"a:contains('$j3pane')\");
-				if (a.length > 0) {
-					a = a[0];
-					var href= a.get('href');
-					jQuery(href)[0].destroy();
+		$document = JFactory::getDocument();
+		$document->addScriptDeclaration($script);
 
-					var accord = a.getParent('.accordion-group');
-					if (typeOf(accord) !== 'null') {
-						accord.destroy();
-					} else {
-						a.destroy();
-					}
-					" . $script . "
-				}
-			});"; */
-
-			$document = JFactory::getDocument();
-			$document->addScriptDeclaration( $script );
-		}
-
-		$close = "function(c){" . $modalid . ".onClose(c);}";
 		$icon = $this->element['icon'] ? '<i class="icon-' . $this->element['icon'] . '"></i> ' : '';
 		$str[] = '<button class="btn" id="' . $modalid . '_button" data-modal="' . $modalid . '">' . $icon . JText::_('JLIB_FORM_BUTTON_SELECT') . '</button>';
 
@@ -174,14 +111,11 @@ class JFormFieldRepeatable extends JFormField
 			$this->value = array_shift($this->value);
 		}
 		$value = htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8');
-		$str[] = '<input type="hidden" name="' . $this->name . '" id="' . $this->id . '" value="' . $value . '" />';
+		$str[] = '<input type="" name="' . $this->name . '" id="' . $this->id . '" value="' . $value . '" />';
 
+		JText::script('JAPPLY');
+		JText::script('JCANCEL');
 		return implode("\n", $str);
 	}
 
-	private function addJs()
-	{
-		JHTML::_('script', 'system/repeatable.js', true, true);
-		return $js;
-	}
 }
